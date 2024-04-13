397c7e64af2767e9373afe40389e9e66248436ee
emily <emilydatascience@gmail.com>
2024-04-13 18:27:59 -0500

    Made lucid prayer work with quick flicker

--- lucid-custom-prayers/lucid-custom-prayers.gradle.kts
+++ lucid-custom-prayers/lucid-custom-prayers.gradle.kts
@@ -1,4 +1,4 @@
-version = "2.1.14"
+version = "2.1.15"
 
 project.extra["PluginName"] = "Lucid Custom Prayers"
 project.extra["PluginDescription"] = "Set up auto prayers based on various event IDs"
--- lucid-custom-prayers/src/main/java/com/lucidplugins/lucidcustomprayers/api/util/CombatUtils.java
+++ lucid-custom-prayers/src/main/java/com/lucidplugins/lucidcustomprayers/api/util/CombatUtils.java
@@ -56,11 +56,20 @@ public class CombatUtils
         MessageUtils.addMessage(client, "Lucid Prayer: Widget container size | " + quickPrayerWidgets.length);
         for (Widget prayerWidget : quickPrayerWidgets)
         {
+            String adjustedPrayerName = prayer.name().toLowerCase().replace('_', ' ').strip();
+            String adjustedWidgetName = prayerWidget.getName().toLowerCase().strip();
             if (!prayerWidget.hasAction("Toggle"))
+            {
+                MessageUtils.addMessage(client,"Lucid Prayer: No toggle | " + adjustedPrayerName + " | " + adjustedWidgetName);
                 continue;
-            if (!prayerWidget.getName().toLowerCase().contains(prayer.name().toLowerCase()))
+            }
+            if (!adjustedWidgetName.contains(adjustedPrayerName))
+            {
+                MessageUtils.addMessage(client,"Lucid Prayer: Not a match | " + adjustedPrayerName + " | " + adjustedWidgetName);
                 continue;
-            MessageUtils.addMessage(client,"Lucid Prayer: Attempting to toggle | " + prayer.name());
+            }
+
+            MessageUtils.addMessage(client,"Lucid Prayer: Attempting to toggle | " + adjustedPrayerName + " | " + adjustedWidgetName);
             invokeAction(client, prayerWidget.getMenu("Toggle"), prayerWidget.getCanvasLocation().getX(), prayerWidget.getCanvasLocation().getY());
         }
     }
