49f40cf3f8e87ae94998d6e7de47995b3d9a5a0f
emily <emilydatascience@gmail.com>
2024-06-13 20:43:47 -0500

    Muspah now tries to move for you

--- lucid-muspah/src/main/java/com/lucidplugins/lucidmuspah/LucidMuspahPlugin.java
+++ lucid-muspah/src/main/java/com/lucidplugins/lucidmuspah/LucidMuspahPlugin.java
@@ -3,7 +3,10 @@ package com.lucidplugins.lucidmuspah;
 import com.google.inject.Provides;
 import com.lucidplugins.lucidmuspah.api.util.CombatUtils;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
+import net.runelite.api.coords.WorldArea;
+import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.AnimationChanged;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.NpcChanged;
@@ -16,9 +19,12 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
 import com.lucidplugins.lucidmuspah.overlay.OverlayMuspah;
+import net.unethicalite.api.entities.NPCs;
+import net.unethicalite.api.movement.Movement;
 import org.pf4j.Extension;
 import javax.inject.Inject;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Extension
@@ -28,6 +34,7 @@ import java.util.List;
         enabledByDefault = false,
         tags = {"muspah"}
 )
+@Slf4j
 public class LucidMuspahPlugin extends Plugin
 {
 
@@ -107,6 +114,29 @@ public class LucidMuspahPlugin extends Plugin
             }
 
             scheduledPrayers.removeIf(prayer -> prayer.getActivationTick() <= client.getTickCount() - 1);
+
+            if (config.stepBackOverlay() && inMeleeForm())
+            {
+                if (!(getTicksUntilAttack() > 1))
+                {
+                    NPC muspah = NPCs.getNearest(x -> x.hasAction("Attack"));
+                    WorldArea muspahAreaExpanded = muspah.getWorldArea().offset(1);
+                    List<WorldPoint> validWalkPoints = muspahAreaExpanded.offset(1).toWorldPointList();
+                    validWalkPoints.removeAll(muspahAreaExpanded.toWorldPointList());
+                    WorldPoint localPoint = client.getLocalPlayer().getWorldLocation();
+                    WorldPoint pointToWalkTo = validWalkPoints.stream().min(Comparator.comparingInt(wp -> wp.distanceTo(localPoint))).orElse(null);
+                    if (pointToWalkTo == null)
+                    {
+                        log.info("pointToWalkTo is null");
+                    }
+                    else
+                    {
+                        log.info("moving to {}", pointToWalkTo.toString());
+                        Movement.walkTo(pointToWalkTo);
+                    }
+                }
+            }
+
         }
     }
 
